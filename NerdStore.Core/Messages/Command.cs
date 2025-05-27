94a0f24fbb5c5ca139a40ded52f9aae4c036b2da
Rafael <rafaelnd2001@gmail.com>
2025-05-27 12:57:23 -0300

    adicionado estrutura para projeto application, e criado teste para o command adicionar pedido item

---
+++ NerdStore.Core/Messages/Command.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+
+namespace NerdStore.Core.Messages
+{
+   public abstract class Command : Message
+   {
+      public DateTime Timestamp { get; private set; }
+      public ValidationResult ValidationResult { get; set; }
+
+      protected Command()
+      {
+         Timestamp = DateTime.Now;
+         ValidationResult = new ValidationResult();
+      }
+
+      public abstract bool EhValido();
+   }
+}
